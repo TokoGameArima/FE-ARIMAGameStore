d0da2e302b0f4784d2c99749f6463ceaedb81b5e
afienzidan <afienzidan227@gmail.com>
2025-06-10 20:56:10 +0700

    feat: setup layouts for main and dashboard pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,40 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Home/Login";
 import Register from "./pages/Home/Register";
 import NotFound from "./pages/NotFound";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import GamesPage from "./pages/Home/GamesPage";
 import CartPage from "./pages/Home/CartPage";
-
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ProtectedRoute from "./routes/ProtectedRoute";
+
+import MainLayout from "./layouts/MainLayout";
+import DashboardLayout from "./layouts/DashboardLayout";
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <div className="p-4">
-        <Routes>
+      <Routes>
+        {/* This section is for Home Pages Layout */}
+        <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/gamelist" element={<GamesPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="*" element={<NotFound />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
+        </Route>
+
+        {/* This section is for Dashboard pages Layout */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout>
                 <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
-      <Footer />
+              </DashboardLayout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="mb-4 text-2xl font-bold">Welcome to Dashboard</h1>
+      <p>This page is protected and doesn't show Navbar or Footer 🎯</p>
+    </div>
+  );
+};
+
+export default Dashboard;
