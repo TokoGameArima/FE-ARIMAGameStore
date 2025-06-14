6a363001343655d5735bb17f7144357270778ae0
afienzidan <afienzidan227@gmail.com>
2025-06-14 18:25:40 +0700

    feat: revised products call api
    - this change also add all CRUD from products

--- src/api/gameApi.js
+++
@@ -1,9 +0,0 @@
-import { api } from "./api";
-
-export const getRandomGames = async () => {
-  const res = await api.get("/api/products");
-  const allGames = res.data;
-
-  const shuffled = allGames.sort(() => 0.5 - Math.random());
-  return shuffled.slice(0, 4);
-};
