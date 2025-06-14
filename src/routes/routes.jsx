aeb6780d6ce13700a2486b883aebaff656cabfee
afienzidan <afienzidan227@gmail.com>
2025-06-14 19:38:45 +0700

    fix: improve routes for previous changes route on layout

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,17 +5,20 @@ import Register from "../pages/Home/Register";
 import NotFound from "../pages/NotFound";
 import GamesPage from "../pages/Dashboard/GamesPage";
 import CartPage from "../pages/Dashboard/CartPage";
-import OrdersPage from "../pages/Dashboard/OrdersPage.jsx";
+import OrdersPage from "../pages/Dashboard/OrdersPage";
 import ReviewPage from "../pages/Dashboard/ReviewPage";
-import Dashboard from "../pages/Dashboard/Dashboard";
-import OrdersAdmin from "../pages/Admin/OrdersAdmin.jsx";
+import DashboardAdmin from "../pages/Admin/DashboardAdmin";
+import OrdersAdmin from "../pages/Admin/OrdersAdmin";
 import OrderDetail from "../pages/Admin/OrderDetail";
 import ProductsAdmin from "../pages/Admin/ProductsAdmin";
 import CategoriesAdmin from "../pages/Admin/CategoriesAdmin";
 import DevelopersAdmin from "../pages/Admin/DevelopersAdmin";
 import Contact from "../pages/Home/Contact";
+
 import MainLayout from "../layouts/MainLayout";
+import AdminLayout from "../layouts/AdminLayout";
 import DashboardLayout from "../layouts/DashboardLayout";
+
 import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
@@ -29,47 +32,27 @@ const AppRoutes = () => {
       </Route>
 
       <Route
-        path="/games"
         element={
           <ProtectedRoute allowedRoles={["user"]}>
-            <GamesPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/cart"
-        element={
-          <ProtectedRoute allowedRoles={["user"]}>
-            <CartPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/orders"
-        element={
-          <ProtectedRoute allowedRoles={["user"]}>
-            <OrdersPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/review/:id"
-        element={
-          <ProtectedRoute allowedRoles={["user"]}>
-            <ReviewPage />
-          </ProtectedRoute>
-        }
-      />
-
-      <Route
-        path="/dashboard"
-        element={
-          <ProtectedRoute allowedRoles={["admin"]}>
             <DashboardLayout />
           </ProtectedRoute>
         }
       >
-        <Route index element={<Dashboard />} />
+        <Route path="/games" element={<GamesPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/review/:id" element={<ReviewPage />} />
+      </Route>
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute allowedRoles={["admin"]}>
+            <AdminLayout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<DashboardAdmin />} />
         <Route path="orders" element={<OrdersAdmin />} />
         <Route path="orders/:id" element={<OrderDetail />} />
         <Route path="products" element={<ProductsAdmin />} />
