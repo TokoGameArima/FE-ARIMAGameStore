8361d26c91036175400576ca41423806e6f05f74
afienzidan <afienzidan227@gmail.com>
2025-06-14 19:56:14 +0700

    feat: create axios interceptors and handle token

--- src/api/api.js
+++ src/api/api.js
@@ -5,3 +5,27 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 export const api = axios.create({
   baseURL: BASE_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.warn("Unauthorized! Token invalid or expired");
+      localStorage.removeItem("token");
+    }
+    return Promise.reject(error);
+  }
+);
