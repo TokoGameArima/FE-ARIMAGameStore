11a3f95f9e5f89339757b4a5c7c65cb2c62f90be
afienzidan <afienzidan227@gmail.com>
2025-06-14 18:26:00 +0700

    chore: change import directory

--- src/components/Home/PopularGames.jsx
+++ src/components/Home/PopularGames.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { getRandomGames } from "../../api/gameApi";
+import { getRandomGames } from "../../api/productApi";
 import { useNavigate } from "react-router-dom";
 
 const PopularGames = () => {
