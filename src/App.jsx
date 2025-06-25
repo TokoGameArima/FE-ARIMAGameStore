c0b5d8517e96978e20273bde883fa59471ff7edb
Raul Maulidhino <raulahmadm314@gmail.com>
2025-06-25 10:10:36 +0700

    feat: import & init AOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { BrowserRouter as Router } from "react-router-dom";
+import React, { useEffect } from "react";
 import AppRoutes from "./routes/routes";
 import { CartProvider } from "./context/CartContext";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+  {/* Initialize AOS Package */}
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  }, []);
+
   return (
     <Router>
       <CartProvider>
